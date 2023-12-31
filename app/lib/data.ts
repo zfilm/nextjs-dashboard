2d052187b331efb4b42a2b4d19e48790fbfcc815
Morten Foyn Bøe <morten.foyn.boe@gmail.com>
2023-12-31 14:56:56 +0100

    Added Pagination on invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -142,7 +142,6 @@ export async function fetchInvoicesPages(query: string) {
       invoices.date::text ILIKE ${`%${query}%`} OR
       invoices.status ILIKE ${`%${query}%`}
   `;
-
     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
     return totalPages;
   } catch (error) {
