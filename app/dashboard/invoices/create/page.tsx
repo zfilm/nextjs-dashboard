7a1ac8dc82aff8fe819458f6c2dde9e34bef135a
Morten Foyn Bøe <morten.foyn.boe@gmail.com>
2024-01-03 18:12:48 +0100

    Eivinds test invoice create

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,6 +1,23 @@
-export default function Page(){
-    return <>
-    <h1>Create</h1>
-    <p>form here..</p>
-</>
+import { fetchCustomers } from "@/app/lib/data"
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs"
+import Form from "@/app/ui/invoices/create-form"
+
+
+export default async function Page(){
+    const customers = await fetchCustomers();
+    return ( 
+        <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+    );
 }
