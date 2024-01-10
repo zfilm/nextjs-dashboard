d655000236ee76f61c8f6801df6ef9c9ce7e984a
Morten Foyn Bøe <morten.foyn.boe@gmail.com>
2024-01-11 00:14:47 +0100

    Added comment to Invoice definitions and added actions.ts

---
+++ app/lib/actions.ts
@@ -0,0 +1,10 @@
+'use server';
+
+export async function createInvoice(formData: FormData) { 
+    const rawFormData = {
+        customerId : formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    };
+    console.log(rawFormData);
+}
