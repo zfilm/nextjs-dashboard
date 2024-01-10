ed7d026e509c088671026800d679c02c1aea6e26
Morten Foyn Bøe <morten.foyn.boe@gmail.com>
2024-01-11 00:53:44 +0100

    Added Zod data validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,21 @@
 'use server';
 
-export async function createInvoice(formData: FormData) { 
-    const rawFormData = {
-        customerId : formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    };
-    console.log(rawFormData);
+import { z } from 'zod';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({id:true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
 }
