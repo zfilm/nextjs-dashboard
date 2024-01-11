ab99dcfa4c491c811bb50b4dc239c34649b1efbd
Morten Foyn Bøe <morten.foyn.boe@gmail.com>
2024-01-11 01:12:47 +0100

    Added RevalidatePath and redirect to invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,11 @@
 'use server';
 
 import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
+
+
 
 const FormSchema = z.object({
     id: z.string(),
@@ -26,4 +30,8 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+
 }
