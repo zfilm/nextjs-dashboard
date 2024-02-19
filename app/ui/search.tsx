8b901e621a3110641935bae77880f7ed22dc57e5
Morten Foyn Bøe <morten.foyn.boe@gmail.com>
2024-02-19 12:43:13 +0100

    Updated comments to search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,19 +6,19 @@ import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
+  const pathname = usePathname(); //${pathname} is the current path, in your case, "/dashboard/invoices"
   const { replace } = useRouter();
 // handleSearch function hånterer søkestrengen med en useDebouncedCallback som er satt til 400. Den setter parameter til query=term ellers sletter query.
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
-    const params = new URLSearchParams(searchParams);
+    const params = new URLSearchParams(searchParams); //URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters. Instead of creating a complex string literal, you can use it to get the params string like ?page=1&query=a.
     params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
-    replace(`${pathname}?${params.toString()}`);
+    replace(`${pathname}?${params.toString()}`); //updates the URL with the user's search data. For example, /dashboard/invoices?query=lee if the user searches for "Lee".
   }, 400);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -32,7 +32,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
           { 
             handleSearch(e.target.value); 
           }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get('query')?.toString()} //3.Keeping the URL and input in sync
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
