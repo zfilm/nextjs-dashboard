b3820be060b3e7b2d3f2637b07bd8909fbddec10
Morten Foyn Bøe <morten.foyn.boe@gmail.com>
2024-03-26 17:43:29 +0100

    13 handling errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -68,10 +68,14 @@ try {
 
 
 export async function deleteInvoice(id: string ) {
+  throw new Error('Failed to Delete Invoice');
+
+// Unreachable code block
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice' };
   } catch (error) {
-      return { message: 'Database Error: Failed to Delete Invoice.' };
+    return { message: 'Database Error: Failed to Delete Invoice' };
   }
-  revalidatePath('/dashboard/invoices');
 }
