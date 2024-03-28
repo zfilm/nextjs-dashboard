7f2b8f2dfbc6c52f5a53fd4357e3a0c69541f529
Morten Foyn Bøe <morten.foyn.boe@gmail.com>
2024-03-28 19:03:24 +0100

    my new customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -3,7 +3,7 @@
 export default function Page({ params }: { params: {  }}){
 
     return <div>
-                <h2>Customers page</h2>
+                <h2>My new Customers page</h2>
 
             </div>;
 }
