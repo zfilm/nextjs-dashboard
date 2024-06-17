478aef9261f60f00d544faf5aa7d4bf5ae02fff1
Morten Foyn Bøe <morten.foyn.boe@gmail.com>
2024-06-17 10:25:51 +0200

    Adding Metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Customer',
+  };
+
 export default function Page({ params }: { params: {  }}){
 
     return <div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | MysAcme Dashboardene' , //%s er title from the page
+  default: 'Default Acme Dashboardet'
+},
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-vercel-1.vercel.app'),
+};
 
 
 export default function RootLayout({
